9a1ad563f545511149750ccfa06232098af6e760
Marco Ghiglieri <ghiglieri@gmail.com>
2018-01-10 01:34:49 +0100

    First Commit
    Not Working, still.

---
+++ mysgw/rootfs/run.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/with-contenv bash
+source /usr/lib/hassio-addons/base.sh
+
+echo "MySensors Gateway"
+MYSGW_TYPE=$(hass.jq "${CONFIG_PATH}" ".type")
+MYSGW_TRN=$(hass.jq "${CONFIG_PATH}" ".transport")
+MQTT_SERVER=$(hass.jq "${CONFIG_PATH}" ".mqtt_server")
+MQTT_CLIENTID=$(hass.jq "${CONFIG_PATH}" ".mqtt_clientid")
+MQTT_TOPIC_IN=$(hass.jq "${CONFIG_PATH}" ".mqtt_topicin")
+MQTT_TOPIC_OUT=$(hass.jq "${CONFIG_PATH}" ".mqtt_topicout")
+
+MQTT_OPTS="--my-mqtt-client-id=$MQTT_CLIENTID --my-controller-url-address=$MQTT_SERVER --my-mqtt-publish-topic-prefix=$MQTT_TOPIC_OUT --my-mqtt-subscribe-topic-prefix=$MQTT_TOPIC_IN"
+
+cd $APPDIR
+./configure --spi-spidev-device=/dev/spidev0.0 --my-transport=$MYSGW_TRN --my-gateway=$MYSGW_TYPE $MQTT_OPTS
+make && make install
+./bin/mysgw -d
