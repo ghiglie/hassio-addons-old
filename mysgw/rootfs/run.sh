4875878126894f260a42ce2e3aba937b5c4fac61
grinnan <cgrinnan@gmail.com>
2019-04-15 02:59:49 -0700

    Update run.sh

--- mysgw/rootfs/run.sh
+++ mysgw/rootfs/run.sh
@@ -8,10 +8,12 @@ MQTT_SERVER=$(hass.jq "${CONFIG_PATH}" ".mqtt_server")
 MQTT_CLIENTID=$(hass.jq "${CONFIG_PATH}" ".mqtt_clientid")
 MQTT_TOPIC_IN=$(hass.jq "${CONFIG_PATH}" ".mqtt_topicin")
 MQTT_TOPIC_OUT=$(hass.jq "${CONFIG_PATH}" ".mqtt_topicout")
+MQTT_USER=$(hass.jq "${CONFIG_PATH}" ".mqtt_user")
+MQTT_PASSWORD=$(hass.jq "${CONFIG_PATH}" ".mqtt_password")
 
-MQTT_OPTS="--my-mqtt-client-id=$MQTT_CLIENTID --my-controller-url-address=$MQTT_SERVER --my-mqtt-publish-topic-prefix=$MQTT_TOPIC_OUT --my-mqtt-subscribe-topic-prefix=$MQTT_TOPIC_IN"
+MQTT_OPTS="--my-mqtt-client-id=$MQTT_CLIENTID --my-controller-url-address=$MQTT_SERVER --my-mqtt-publish-topic-prefix=$MQTT_TOPIC_OUT --my-mqtt-subscribe-topic-prefix=$MQTT_TOPIC_IN --my-mqtt-password=password --my-mqtt-user=user"
 
 cd $APPDIR
 ./configure --spi-spidev-device=/dev/spidev0.0 --my-transport=$MYSGW_TRN --my-gateway=$MYSGW_TYPE $MQTT_OPTS
-make && make install
-./bin/mysgw --daemon
+make -j4 && make install
+sudo./bin/mysgw --daemon
