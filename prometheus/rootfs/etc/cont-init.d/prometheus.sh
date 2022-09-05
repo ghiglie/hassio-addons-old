2d9f01b1c429b57a50349859e82ddc5928cc021a
grinnan <cgrinnan@gmail.com>
2022-09-05 15:09:17 -0700

    add prometheus from 42Network

---
+++ prometheus/rootfs/etc/cont-init.d/prometheus.sh
@@ -0,0 +1,11 @@
+#!/command/with-contenv bashio
+# ==============================================================================
+# Home Assistant Community Add-on: Prometheus
+# Configures Prometheus
+# ==============================================================================
+echo "${SUPERVISOR_TOKEN}" > '/run/home-assistant.token'
+
+mkdir -p /data/prometheus
+chown -R prometheus:prometheus /data/prometheus
+find /data/prometheus -not -perm 0644 -type f -exec chmod 0660 {} \;
+find /data/prometheus -not -perm 0755 -type d -exec chmod 0770 {} \;
